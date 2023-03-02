13937d0aeb3d4fa356fb44009613eccee60e5e68
João Alberto Conti Carreira Martins <joaoalbertomartinsbb@gmail.com>
2023-03-02 11:37:55 -0300

    Leitura dos arquivos jason e exibição na index

--- Pokedex/Controllers/HomeController.cs
+++ Pokedex/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Text.Json; 
 using Microsoft.AspNetCore.Mvc;
 using Pokedex.Models;
 
@@ -15,29 +16,13 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        Pokemon bulba = new Pokemon();
-        bulba.Numero = 1; 
-        bulba.Nome = "Bulbassaur";
-        bulba.Tipo.Add("Planta");
-        bulba.Tipo.Add("Venenoso"); 
-        bulba.Imagem = "\\img\\pokemons\\001.png"; 
-        // bulba.Tipo.AddRange(new string[] {"Planta", "Venenoso"}); <----- outro jeito de definir o tipo  
-        
-
-        Pokemon ivy = new()
+        string arquivo = @"Data\pokemons.json";
+        using (StreamReader leitor = new StreamReader(arquivo))
         {
-           Numero = 2,
-           Nome = "Ivysaur",
-           Tipo = {"Planta", "Venenoso"},
-           Imagem = "\\img\\pokemons\\002.png"
-        };
-
-        var venu = new Pokemon();
-
-        ViewData["Ivysaur"] = ivy; 
-
-
-        return View(bulba);
+            string dados = leitor.ReadToEnd();
+            var pokemons = JsonSerializer.Deserialize<List<Pokemon>>(dados);
+            return View(pokemons); 
+        }
     }
 
     public IActionResult Privacy()
